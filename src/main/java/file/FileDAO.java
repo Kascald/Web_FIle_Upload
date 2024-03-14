2d14e76e957f0cf7b404c85f57ca0b87ebb68add
YRN <118609415+Kascald@users.noreply.github.com>
2024-03-14 22:39:01 +0900

    DAO - Upload method add

--- src/main/java/file/FileDAO.java
+++ src/main/java/file/FileDAO.java
@@ -2,6 +2,7 @@ package file;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class FileDAO {
 
@@ -18,5 +19,21 @@ public class FileDAO {
             e.printStackTrace();
         }
     }
+
+    public int upload(String fileName , String fileRealName) {
+        PreparedStatement pstmt = null;
+        String SQL = "INSERT INTO file values(? , ?)";
+        try {
+            pstmt = conn.prepareStatement(SQL);
+            pstmt.setString(1, fileName);
+            pstmt.setString(2, fileRealName);
+            return pstmt.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    
 
 }
